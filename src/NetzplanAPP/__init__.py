97835ef8b5126d515d15ad68ec462c0f14ccec27
Sebastian Meisel <sebastian.meisel@gmail.com>
2021-12-01 21:39:24 +0100

    Flask-App erstellt. Vorbereitung für automatisches Testen.

---
+++ src/NetzplanAPP/__init__.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import os                                                             # Zum Speichern
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory  # Webserver
+from werkzeug.utils import secure_filename                            # Begrenze erlaubte Dateien 
+from netzplan.netzplan import Projekt, Netzplan                                # Generiere Netzplan
+
+# Create an App
+def create_app(test_config=None):
+    # Web-App initialisieren
+    app = Flask(__name__, instance_relative_config=True)
+    ALLOWED_EXTENSIONS = {'xlsx'}
+
+    # Konfiguration
+    app.config.from_mapping(
+            SECRET_KEY='dev', 
+            UPLOAD_FOLDER = "./static/file/uploads" # Upload-Ordner für Datei-Upload
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Funktion zum überprüfen. ob Dateityp erlaubt ist
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
+    # Funktion zum Verarbeiten des Dateiuploads
+    @app.route('/', methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == 'POST':
+            # Überprüfe, ob Daei ausgewählt wurde
+            if 'file' not in request.files:
+                return render_template('index.html', error_message='Keine Datei gewählt')
+            file = request.files['file']
+            # Dateiname darf kein leere String sein
+            if file.filename == '':
+                return render_template('index.html', error_message='Keine Datei gewählt')
+            # Nur weiterarbeiten, wenn der Dateiname "sicher" ist
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+                ##### Netzplan aus Datei generieren
+                P = Projekt(1, "Projekt")
+                error_message = P.ImportiereVonExcel(str(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+    
+                if error_message != "":
+                    return render_template('index.html', error_message=error_message)
+                    
+    
+                NP = Netzplan("Projekt")
+                NP.Zeichnen(P)
+                NP.PdfExport(app.config['UPLOAD_FOLDER'])
+                NP.JPGExport(app.config['UPLOAD_FOLDER'])
+                
+                return render_template('netzplan.html')
+            else:
+                return render_template('index.html', error_message='Bitte Dateinamen ändern!')
+    
+        return render_template('index.html')
+    
+    
+    
+    
+    @app.route('/uploads/<name>')
+    def download_file(name):
+        return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+    return app
+    
