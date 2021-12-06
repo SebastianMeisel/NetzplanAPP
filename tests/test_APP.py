266d612d5078264cc642fc5d30ede5898615803d
Sebastian Meisel <sebastian.meisel@gmail.com>
2021-12-06 17:42:00 +0100

    black-Code-Formatter und pre-commit hinzugefügt.

--- tests/test_APP.py
+++ tests/test_APP.py
@@ -8,39 +8,62 @@ import html
 # Client einrichten
 @pytest.fixture
 def client():
-    app = create_app({'TESTING': True})
+    app = create_app({"TESTING": True})
 
     with app.test_client() as client:
         yield client
 
 
-# Hilfsfunktion        
+# Hilfsfunktion
 def send_xlsx(client, Pfad: str):
-    with open(Pfad, 'rb') as file:
-        return client.post('/', data=dict(file=(file, file.name)), follow_redirects=True, content_type='multipart/form-data')
+    with open(Pfad, "rb") as file:
+        return client.post(
+            "/",
+            data=dict(file=(file, file.name)),
+            follow_redirects=True,
+            content_type="multipart/form-data",
+        )
 
-@pytest.mark.parametrize("datei,expected", [
-    ("Projekt.xlsx", 'Netzplan-Download'),
-    ("Projekt_Ohne_Ressourcen.xlsx", 'Netzplan-Download'),
-    ("Projekt_Ohne_Ressourcen_und_ID_num.xlsx", 'Netzplan-Download'),
-    ("Projekt_Ohne_Ressourcen_und_ID.xlsx", 'Spalte &#39;ID&#39; fehlt in der Tabelle &#39;Projekt&#39;!'),
-    ("Projekt_Ohne_Ressourcen_und_ID_Dauer.xlsx", 'Spalte &#39;ID&#39; fehlt in der Tabelle &#39;Projekt&#39;!'),
-    ("Projekt_Ohne_Projekt.xlsx", 'Tabelle &#39;Projekt&#39; fehlt oder hat den falschen Namen'),
-    ("Ressourcen_Ohne_ID.xlsx", 'Spalte &#39;ID&#39; fehlt in der Tabelle &#39;Ressourcen&#39;!'),
-])
-    
 
+@pytest.mark.parametrize(
+    "datei,expected",
+    [
+        ("Projekt.xlsx", "Netzplan-Download"),
+        ("Projekt_Ohne_Ressourcen.xlsx", "Netzplan-Download"),
+        ("Projekt_Ohne_Ressourcen_und_ID_num.xlsx", "Netzplan-Download"),
+        (
+            "Projekt_Ohne_Ressourcen_und_ID.xlsx",
+            "Spalte &#39;ID&#39; fehlt in der Tabelle &#39;Projekt&#39;!",
+        ),
+        (
+            "Projekt_Ohne_Ressourcen_und_ID_Dauer.xlsx",
+            "Spalte &#39;ID&#39; fehlt in der Tabelle &#39;Projekt&#39;!",
+        ),
+        (
+            "Projekt_Ohne_Projekt.xlsx",
+            "Tabelle &#39;Projekt&#39; fehlt oder hat den falschen Namen",
+        ),
+        (
+            "Ressourcen_Ohne_ID.xlsx",
+            "Spalte &#39;ID&#39; fehlt in der Tabelle &#39;Ressourcen&#39;!",
+        ),
+    ],
+)
 def test_send_xlsx(client, datei, expected):
 
     # Ordentliche Testdatei
     Pfad = join(dirname(realpath(__file__)), datei)
     rv = send_xlsx(client, Pfad)
-    assert expected in rv.data.decode('utf-8')
+    assert expected in rv.data.decode("utf-8")
 
-    
+
 def test_keine_datei(client):
     # keine Datei
-    rv = client.post('/', data=dict(file=''), follow_redirects=True, content_type='multipart/form-data')
+    rv = client.post(
+        "/",
+        data=dict(file=""),
+        follow_redirects=True,
+        content_type="multipart/form-data",
+    )
 
-    assert 'Keine Datei gewählt' in rv.data.decode('utf-8') # "Keine Datei gewählt"
-    
+    assert "Keine Datei gewählt" in rv.data.decode("utf-8")  # "Keine Datei gewählt"
---
+++ tests/test_py_only_Project.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+from netzplan import Projekt, Netzplan
+import hashlib
+
+
+def sha256sum(filename):
+    h = hashlib.sha256()
+    b = bytearray(128 * 1024)
+    mv = memoryview(b)
+    with open(filename, "rb", buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
+            if n > 2:
+                h.update(mv[:n])
+    return h.hexdigest()
+
+
+def test_project(capsys):
+    ###################################################################
+    # Beispiel
+
+    P1 = Projekt(1, "Beispiel")
+
+    # Arbeits-Pakete: Bezeichnung, Aufwand, ID (optional)
+    P1.NeuesArbeitsPaket("Anfangen", 5, "1.1.1"),
+    P1.NeuesArbeitsPaket("Weitermachen", 7, "1.1.2")
+    P1.NeuesArbeitsPaket("Einkaufen", 1, "1.1.3")
+    P1.NeuesArbeitsPaket("Ganz anders weitermachen", 4, "1.2.2")
+    P1.NeuesArbeitsPaket("Aufhören", 7, "1.2.3")
+    P1.NeuesArbeitsPaket("Anders weitermachen", 5, "1.2.1")
+    P1.NeuesArbeitsPaket("Pause", 8, "1.3.1")
+    P1.NeuesArbeitsPaket("Erholung", 6, "1.3.2")
+    P1.NeuesArbeitsPaket("Feiern", 6, "1.3.3")
+
+    # Abhängigkeiten
+    P1.ArbeitsPakete["1.1.2"].Folgt("1.1.1")
+    P1.ArbeitsPakete["1.1.3"].Folgt("1.1.2")
+    P1.ArbeitsPakete["1.2.1"].Folgt("1.1.1")
+    P1.ArbeitsPakete["1.2.2"].Folgt("1.2.1")
+    P1.ArbeitsPakete["1.2.3"].Folgt("1.2.2")
+    P1.ArbeitsPakete["1.3.1"].Folgt("1.2.1")
+    P1.ArbeitsPakete["1.3.2"].Folgt("1.3.1")
+    P1.ArbeitsPakete["1.3.3"].Folgt(
+        ["1.1.3", "1.2.3", "1.3.2"]
+    )  # Mehrere Arbeitspackete in einer Liste
+    # (in eckigen Klammern) zusammenfassen
+    ################################################################################
+    # Ressourcen: Name
+    P1.NeueRessource("FM", "Frank Müller")
+    P1.NeueRessource("PL", "Pipi Langstrumpf")
+
+    # Hr. Meier:  Ressource, Arbeitspacket, Kapazität (Default 100%)
+    P1.RessourceZuweisen("FM", "1.1.1")
+    P1.RessourceZuweisen("FM", "1.1.2", 50)
+    P1.RessourceZuweisen("FM", "1.2.1", 50)
+    P1.RessourceZuweisen("FM", "1.2.3")
+    P1.RessourceZuweisen("FM", "1.1.3")
+    P1.RessourceZuweisen("FM", "1.3.2")
+    P1.RessourceZuweisen("FM", "1.3.3")
+
+    # Pipi: Ressource, Arbeitspacket, Kapazität (Default 100%)
+    P1.RessourceZuweisen("PL", "1.1.1")
+    P1.RessourceZuweisen("PL", "1.1.3")
+    P1.RessourceZuweisen("PL", "1.2.1")
+    P1.RessourceZuweisen("PL", "1.2.2", 50)
+    P1.RessourceZuweisen("PL", "1.3.1", 50)
+    P1.RessourceZuweisen("PL", "1.3.2")
+    P1.RessourceZuweisen("PL", "1.3.3")
+
+    # Netzplan: Name der PDF
+    N1 = Netzplan("Netzplan")
+    N1.Zeichnen(P1)
+    N1.PdfExport()
+    N1.JPGExport()
+
+    # Generiere MD5-Hash
+    sum = sha256sum("Netzplan.jpg")
+    assert sum == "c588730431ef036c3049e98042df675b6feb0ddc32b8723d0470bf792e9b9629"
+
+    # Kristischen Pfad ausgeben
+    P1.ZeigeKritischenPfad()
+    assert (
+        capsys.readouterr().out
+        == "Kritischer Pfad: [ 1.1.1  - 1.2.1  - 1.3.1  - 1.3.2  - 1.3.3 ]\n"
+    )
