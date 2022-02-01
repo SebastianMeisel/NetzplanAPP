9ed5ff51e1751dde332dccc71283187dcd49177c
Sebastian Meisel <sebastian.meisel@gmail.com>
2022-02-01 21:11:56 +0100

    Datensicherheit: Lösche verwaiste Dateien im Uploadfolder

--- src/NetzplanAPP/__init__.py
+++ src/NetzplanAPP/__init__.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
-import os# Zum Speichern
+import os  # Zum Speichern
 from os.path import join, dirname, realpath
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory  # Webserver
-from werkzeug.utils import secure_filename                            # Begrenze erlaubte Dateien 
-from netzplan.netzplan import Projekt, Netzplan                                # Generiere Netzplan
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)  # Webserver
+from werkzeug.utils import secure_filename  # Begrenze erlaubte Dateien
+from netzplan.netzplan import Projekt, Netzplan  # Generiere Netzplan
 
 # Create an App
 def create_app(test_config=None):
     # Web-App initialisieren
     app = Flask(__name__, instance_relative_config=True)
-    ALLOWED_EXTENSIONS = {'xlsx'}
+    ALLOWED_EXTENSIONS = {"xlsx"}
 
     # Konfiguration
     app.config.from_mapping(
-            SECRET_KEY='dev', 
-            UPLOAD_FOLDER = join(dirname(realpath(__file__)),"./static/file/uploads") # Upload-Ordner für Datei-Upload
+        SECRET_KEY="dev",
+        UPLOAD_FOLDER=join(
+            dirname(realpath(__file__)), "./static/file/uploads"
+        ),  # Upload-Ordner für Datei-Upload
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -32,50 +41,61 @@ def create_app(test_config=None):
 
     # Funktion zum überprüfen. ob Dateityp erlaubt ist
     def allowed_file(filename):
-        return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
+        return (
+            "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+        )
+
     # Funktion zum Verarbeiten des Dateiuploads
-    @app.route('/', methods=['GET', 'POST'])
+    @app.route("/", methods=["GET", "POST"])
     def upload_file():
-        if request.method == 'POST':
+        # LÃ¶sche verwaist eDateien im Upload-Ordner
+        for file in os.listdir(app.config["UPLOAD_FOLDER"]):
+            os.unlink(os.path.join(app.config["UPLOAD_FOLDER"], file))
+        if request.method == "POST":
             # Überprüfe, ob Daei ausgewählt wurde
-            if 'file' not in request.files:
-                return render_template('index.html', error_message='Keine Datei gewählt')
-            file = request.files['file']
+            if "file" not in request.files:
+                return render_template(
+                    "index.html", error_message="Keine Datei gewählt"
+                )
+            file = request.files["file"]
             # Dateiname darf kein leere String sein
-            if file.filename == '':
-                return render_template('index.html', error_message='Keine Datei gewählt')
+            if file.filename == "":
+                return render_template(
+                    "index.html", error_message="Keine Datei gewählt"
+                )
             # Nur weiterarbeiten, wenn der Dateiname "sicher" ist
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
                 ##### Netzplan aus Datei generieren
                 P = Projekt(1, "Projekt")
-                error_message = P.ImportiereVonExcel(str(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
-    
+                error_message = P.ImportiereVonExcel(
+                    str(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+                )
+
                 if error_message != "":
-                    return render_template('index.html', error_message=error_message)
-                    
-    
+                    return render_template("index.html", error_message=error_message)
+
+                # LÃ¶sche Pdf
+                os.unlink(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+                # Ausgabedateien erstellen
                 NP = Netzplan("Projekt")
                 NP.Zeichnen(P)
-                NP.PdfExport(app.config['UPLOAD_FOLDER'])
-                NP.JPGExport(app.config['UPLOAD_FOLDER'])
-                
-                return render_template('netzplan.html')
+                NP.PdfExport(app.config["UPLOAD_FOLDER"])
+                NP.JPGExport(app.config["UPLOAD_FOLDER"])
+
+                return render_template("netzplan.html")
             else:
-                return render_template('index.html', error_message='Bitte Dateinamen ändern!')
-    
-        return render_template('index.html')
-    
-    
-    
-    
-    @app.route('/uploads/<name>')
+                return render_template(
+                    "index.html", error_message="Bitte Dateinamen ändern!"
+                )
+
+        return render_template("index.html")
+
+    @app.route("/uploads/<name>")
     def download_file(name):
         return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
     return app
-    
